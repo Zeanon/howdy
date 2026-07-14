ee3a98a0fe2d74da058b63ae7ba591c4f3ea2368
Zeanon <thezeanon@gmail.com>
2026-07-14 20:53:37 +0200

    .

--- howdy-cli/install-cli.sh
+++ howdy-cli/install-cli.sh
@@ -4,7 +4,7 @@ echo "Installing howdy-cli"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cat bin/howdy-cli.in | sudo tee /usr/local/bin/howdy-cli > /dev/null
+sudo cp bin/howdy-cli.in /usr/local/bin/howdy-cli
 echo "$SCRIPT_DIR/howdy-manager.sh" | sudo tee -a /usr/local/bin/howdy-cli > /dev/null
 
 sudo chmod 755 /usr/local/bin/howdy-cli
