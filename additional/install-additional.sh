b0063a10df9dee4a9c4bc193b8857a4517c58c6e
Zeanon <thezeanon@gmail.com>
2026-07-14 20:49:50 +0200

    .

--- additional/install-additional.sh
+++ additional/install-additional.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-read -n1 -p "Install howdy-gtk desktop file? [y,n]" howdy_gtk
-echo
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+read -p "Install howdy-gtk desktop file? [Y|n]" howdy_gtk
+howdy_gtk="${howdy_gtk:=Y}"
 case $howdy_gtk in
-    y|Y) cp Howdy.desktop ~/.local/share/applications/Howdy.desktop && echo "Created howdy-gtk desktop file" ;;
+    y|Y) cp Howdy.desktop ~/.local/share/applications/Howdy.desktop && echo "Icon=${SCRIPT_DIR%/*}/howdy-gtk/src/logo.png" | tee -a ~/.local/share/applications/Howdy.desktop > /dev/null && echo "Created howdy-gtk desktop file" ;;
     *) echo "Did not create howdy-gtk desktop file" ;;
 esac
 echo
 
-read -n1 -p "Install kde pam file? [y,n]" kde
-echo
+read -p "Install kde pam file? [y|N]" kde
+kde="${kde:=N}"
 case $kde in
     y|Y) sudo cp kde /etc/pam.d/kde && echo "Installed kde pam file" ;;
     *) echo "Did not install kde pam file" ;;
 esac
 echo
 
-read -n1 -p "Install polkit pam file? [y,n]" polkit
-echo
+read -p "Install polkit pam file? [y|N]" polkit
+polkit="${polkit:=N}"
 case $polkit in
     y|Y) sudo cp polkit-1 /etc/pam.d/polkit-1 && echo "Installed polkit pam file" ;;
     *) echo "Did not install polkit pam file" ;;
--- howdy-cli/install-cli.sh
+++ howdy-cli/install-cli.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 echo "Installing howdy-cli"
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-sudo ln -s $SCRIPT_DIR/howdy-manager.sh /usr/local/bin/howdy-cli
+
+cat bin/howdy-cli.in | sudo tee /usr/local/bin/howdy-cli > /dev/null
+echo "$SCRIPT_DIR/howdy-manager.sh" | sudo tee -a /usr/local/bin/howdy-cli > /dev/null
+
+sudo chmod 755 /usr/local/bin/howdy-cli
+
+echo "Done"
